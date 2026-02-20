892193b4b836b5a820ebab240dba5c7d720080c1
Shunting Zhang <shunting@fb.com>
2026-02-20 14:37:33 -0800

    WIP: helion distributed kernel autotuning
    stack-info: PR: https://github.com/pytorch/helion/pull/1532, branch: shunting314/stack/3

--- test/test_distributed.py
+++ test/test_distributed.py
@@ -17,8 +17,65 @@ from helion._testing import skipIfXPU
 from helion._utils import sync_seed
 
 
+import torch
+import torch.distributed as dist
+import contextlib
+import unittest
+import os
+from torch.testing._internal.common_distributed import MultiProcessTestCase
+from datetime import timedelta
+from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
+from torch.testing._internal.common_utils import instantiate_parametrized_tests
+from torch.testing._internal.common_utils import parametrize
+from torch.testing._internal.common_utils import run_tests
+from helion._testing import onlyBackends
+from torch.utils.cpp_extension import load_inline
+import helion.language as hl
+import helion
+from helion._utils import sync_seed
+from helion._testing import TestCase
+from helion._testing import skipIfRocm
+from helion._testing import skipIfXPU
+from helion._testing import code_and_output
+from helion._testing import all_gather_object
+import torch.distributed._symmetric_memory as symm_mem
+
+def one_shot_all_reduce_kernel(
+    a_shared: torch.Tensor,
+    my_rank: hl.constexpr,
+    group_name: hl.constexpr,
+    WORLD_SIZE: hl.constexpr,
+) -> torch.Tensor:
+    out = torch.empty_like(a_shared)
+    N = out.size(0)
+    a_shared_tuple = torch.ops.symm_mem.get_remote_tensors(a_shared, group_name)
+
+    for tile_n in hl.tile(N):
+        acc = hl.zeros([tile_n], dtype=a_shared.dtype, device=a_shared.device)
+
+        for a in a_shared_tuple:
+            acc += a[tile_n]
+
+        out[tile_n] = acc
+    return out
+
+
 @onlyBackends(["triton"])
+@instantiate_parametrized_tests
 class TestDistributed(TestCase, MultiProcessTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls._class_stack = contextlib.ExitStack()
+        cls._class_stack.enter_context(unittest.mock.patch.dict(os.environ, {
+            "CHECK_CONFIG_CONSISTANCY": "1",
+        }))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls._class_stack.close()
+        super().tearDownClass()
+
     def setUp(self) -> None:
         super().setUp()
         self._spawn_processes()
@@ -83,6 +140,70 @@ class TestDistributed(TestCase, MultiProcessTestCase):
 
         self._cleanup_process()
 
+   
+
+    @skipIfRocm("Distributed example requires CUDA/NCCL")
+    @skipIfXPU("Distributed operations require CCL, not yet fully integrated")
+    @skip_if_lt_x_gpu(4)
+    @parametrize("autotuner", ["fixed", "PatternSearch", "LFBOPatternSearch", "LFBOTreeSearch", "DifferentialEvolutionSearch", "DESurrogateHybrid", "FiniteSearch", "RandomSearch"])
+    def test_all_reduce(self, autotuner):
+        self._init_process()
+        if autotuner == "fixed":
+            kernel = helion.kernel(
+                config=helion.Config(
+                    block_sizes=[8192],
+                    num_warps=32,
+                ),
+            )(one_shot_all_reduce_kernel)
+            context = contextlib.nullcontext()
+        elif autotuner == "FiniteSearch":
+            kernel = helion.kernel(
+                configs=[
+                    helion.Config(
+                        block_sizes=[8192],
+                        num_warps=32
+                    ),
+                    helion.Config(
+                        block_sizes=[4096],
+                        num_warps=32
+                    ),
+                ]
+            )(one_shot_all_reduce_kernel)
+            context = unittest.mock.patch.dict(os.environ, {"HELION_AUTOTUNER": autotuner})
+        else:
+            kernel = helion.kernel(one_shot_all_reduce_kernel)
+            context = unittest.mock.patch.dict(os.environ, {"HELION_AUTOTUNER": autotuner})
+
+        with context:
+            self.do_test_all_reduce(kernel)
+
+        self._cleanup_process()
+
+    def do_test_all_reduce(self, kernel):
+        group = dist.group.WORLD
+
+        N = 16384
+        dtype = torch.bfloat16
+
+        a_shared = symm_mem.empty(
+            N, dtype=dtype, device=self.device
+        ).normal_()
+
+        symm_mem_hdl = symm_mem.rendezvous(a_shared, group=group)
+
+        result = kernel(
+            a_shared,
+            symm_mem_hdl.rank,
+            group.group_name,
+            symm_mem_hdl.world_size,
+        )
+
+        torch.cuda.synchronize()
+
+        expected = torch.empty_like(result).copy_(a_shared)
+        dist.all_reduce(expected, op=dist.ReduceOp.SUM)
+
+        torch.testing.assert_close(result, expected, rtol=1e-1, atol=1e-1)
 
 if __name__ == "__main__":
     run_tests()
